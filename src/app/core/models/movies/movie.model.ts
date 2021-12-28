e7f18f34e633460e02abba2f94c5e61936ec6e29
jada183 <jada183.jf@gmail.com>
2021-12-28 11:13:02 +0100

    Add models to http response

---
+++ src/app/core/models/movies/movie.model.ts
@@ -0,0 +1,10 @@
+export class Movie {
+    id: number;
+    title: string;
+    poster: string;
+    genre: Array<string>;
+    year: number;
+    duration: number;
+    imdbRating: number;
+    actors: Array<number>;
+}
