81f2d608c890690d4a487af6cd044b2187799495
jada183 <jada183.jf@gmail.com>
2021-12-28 12:43:16 +0100

    Add interceptor

---
+++ src/app/core/http-interceptor/http-common-interceptor.ts
@@ -0,0 +1,28 @@
+import { HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, filter, map } from 'rxjs/operators';
+import { SpinnerFullOverlayService } from '../spinner-full-overlay/spinner-full-overlay.service';
+
+// XXX: declare this interceptor on main module as HTTP interceptor
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpCommonInterceptor implements HttpInterceptor {
+  // TODO: write tests
+  constructor(
+    private readonly spinnerFullOverlayService: SpinnerFullOverlayService
+  ) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    this.spinnerFullOverlayService.show();
+    const cloneReq = req.clone({});
+    return next.handle(cloneReq).pipe(
+      filter((res) => res !== undefined),
+      map((res) => {
+        this.spinnerFullOverlayService.hide();
+        return res;
+      })
+    );
+  }
+}
